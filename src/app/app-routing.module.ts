427cfdf05e5c8aa1e1aa520562d8f42aa7d4a693
Niklas Deutschmann <nsdn@posteo.de>
2020-03-28 11:33:57 +0100

    Removed tracing for router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: true }), HttpClientModule],
+  imports: [RouterModule.forRoot(routes), HttpClientModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
